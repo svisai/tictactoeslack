4a89bfeeee5f68db5cbcfd90bc6f99a4079d22ac
Samidha Visai <svisai@umich.edu>
2016-10-09 19:32:24 -0400

    initializing data

--- app.py
+++ app.py
@@ -32,25 +32,25 @@ def main():
     
     cursor = app.mysql.connection.cursor()
     
-    exist = cursor.execute("SELECT team_id FROM team WHERE team_key = '{0}'".format(teamkey))
-    if exist.fetchone() is None:
+    cursor.execute("SELECT team_id FROM team WHERE team_key = '{0}'".format(teamkey))
+    if cursor.fetchone() is None:
         cursor.execute("INSERT INTO team (team_key, team_domain) VALUES ('{0}', '{1}')".format(teamkey, team_domain))
 
     teamid = cursor.execute("SELECT team_id FROM team WHERE team_key = '{0}'".format(teamkey))
 
-    exist = cursor.execute("SELECT * FROM channel WHERE channel_key = '{0}'".format(channelkey))
-    if exist.fetchone() is None:
+    cursor.execute("SELECT * FROM channel WHERE channel_key = '{0}'".format(channelkey))
+    if cursor.fetchone() is None:
         cursor.execute("INSERT INTO channel (channel_key, team_id, channel_name) VALUES ('{0}', '{1}', '{2}')".format(channelkey, teamid, channel_name))
 
-    exist = cursor.execute("SELECT * FROM player WHERE player_key = '{0}'".format(userkey))
-    if exist.fetchone() is None:
+    cursor.execute("SELECT * FROM player WHERE player_key = '{0}'".format(userkey))
+    if cursor.fetchone() is None:
         cursor.execute("INSERT INTO player (player_key, total_wins, total_losses, total_ties, team_id, player_name) VALUES ('{0}', {1}, {2}, '{3}', '{4}')".format(userkey, 0, 0, 0, teamid, user_name))
     
     channelid = cursor.execute("SELECT channel_id FROM channel WHERE team_id = '{0}' AND channel_key = '{1}'".format(teamid, channelkey))
     startplayer = cursor.execute("SELECT player_id FROM player WHERE team_id = '{0}' AND player_KEY = '{1}'".format(teamid, userkey))
 
-    exist = cursor.execute("SELECT * FROM game WHERE channel_id = '{0}' AND start_player = '{1}'".format(channelid, startplayer))
-    if exist.fetchone() is None:
+    cursor.execute("SELECT * FROM game WHERE channel_id = '{0}' AND start_player = '{1}'".format(channelid, startplayer))
+    if cursor.fetchone() is None:
         cursor.execute("INSERT INTO game (channel_id, start_time, end_time, start_player, board_size, game_board, time_limit_move, time_limit_game, result_id, max_players, total_number_moves) VALUES ('{0}', NOW(), '{2}', '{3}', {4}, '{5}', {6}, {7}, {8}, {9})".format(channelid, NULL, startplayer, 3, '000000000',5, 120, 0, 2, 0))
 
     cursor.close()
