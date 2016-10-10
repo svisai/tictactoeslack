8e534402dc2881c7d9373d37ab8e2060a5ec7bcf
Samidha Visai <svisai@umich.edu>
2016-10-10 12:44:11 -0400

    security, json responses

--- app.py
+++ app.py
@@ -49,17 +49,18 @@ def main():
     else:
         return 'Invalid command for tic tac toe. Use /ttt help for info'
 def help():
-    return "test"
-        #{
-        #   "response_type": "ephemeral",
-        #   "text": "How to use /ttt",
-        #   "attachments":[
-        #       {
-        #        "text":"To start a game: /ttt start @user\n To make a move: /ttt move [position from 0 to 8]\n To end game: /ttt forfeit\n To display board: /ttt status"
-        #       }
-        #   ]
-#
-#}
+    return
+    {
+            "Content-type": "application/json",
+            "response_type": "ephemeral",
+            "text": "How to use /ttt",
+            "attachments":[
+                {
+                 "text":"To start a game: /ttt start @user\n To make a move: /ttt move [position from 0 to 8]\n To end game: /ttt forfeit\n To display board: /ttt status"
+                }
+            ]
+        
+    }
 
 def move(teamkey, channelkey, userkey, position):
     cursor = app.mysql.connection.cursor()
