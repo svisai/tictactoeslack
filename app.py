9d09727a405ced9bdcc5039a5f39416249235d92
Samidha Visai <svisai@umich.edu>
2016-10-10 20:58:38 -0400

    ending game with a win

--- app.py
+++ app.py
@@ -121,10 +121,10 @@ def move(teamkey, channelkey, userkey, position):
     win = 0
     if(column == row):
         if(num_moves % 2 == 0):
-            cursor.execute("UPDATE game SET row0=row0+1 WHERE game_id={0}".format(gameid))
+            cursor.execute("UPDATE game SET row0=row0+1 WHERE game_id={0}".format(gameid[0]))
         else:
-            cursor.execute("UPDATE game SET row0=row0-1 WHERE game_id={0}".format(gameid))
-        cursor.execute("SELECT diag0 FROM game WHERE game_id={1}".format(row, gameid))
+            cursor.execute("UPDATE game SET row0=row0-1 WHERE game_id={0}".format(gameid[0]))
+        cursor.execute("SELECT diag0 FROM game WHERE game_id={1}".format(row, gameid[0]))
         res = cursor.fetchone()
         if(num_moves % 2 == 0 and res[0] == board_size):
             win = 1
@@ -134,10 +134,10 @@ def move(teamkey, channelkey, userkey, position):
 
     elif(column == boardsize - row - 1):
         if(num_moves % 2 == 0):
-            cursor.execute("UPDATE game SET row1=row1+1 WHERE game_id={0}".format(gameid))
+            cursor.execute("UPDATE game SET row1=row1+1 WHERE game_id={0}".format(gameid[0]))
         else:
-            cursor.execute("UPDATE game SET row1=row1-1 WHERE game_id={0}".format(gameid))
-        cursor.execute("SELECT diag1 FROM game WHERE game_id={1}".format(row, gameid))
+            cursor.execute("UPDATE game SET row1=row1-1 WHERE game_id={0}".format(gameid[0]))
+        cursor.execute("SELECT diag1 FROM game WHERE game_id={1}".format(row, gameid[0]))
         res = cursor.fetchone()
         if(num_moves % 2 == 0 and res[0] == board_size):
             win = 1
@@ -147,20 +147,20 @@ def move(teamkey, channelkey, userkey, position):
     if(num_moves % 2 == 0):
         row = "row" + str(row)
         print(row)
-        cursor.execute("UPDATE game SET '{0}'='{1}'+1 WHERE game_id={2}".format(row, row, gameid))
-        cursor.execute("UPDATE game SET column{0}=column{1}+1 WHERE game_id={2}".format(column, column, gameid))
+        cursor.execute("UPDATE game SET {0}={1}+1 WHERE game_id={2}".format(row, row, gameid[0]))
+        cursor.execute("UPDATE game SET column{0}=column{1}+1 WHERE game_id={2}".format(column, column, gameid[0]))
     else:
-        cursor.execute("UPDATE game SET row{0}=row{1}-1 WHERE game_id={2}".format(row, row, gameid))
-        cursor.execute("UPDATE game SET column{0}=column{1}-1 WHERE game_id={2}".format(column, column, gameid))
+        cursor.execute("UPDATE game SET row{0}=row{1}-1 WHERE game_id={2}".format(row, row, gameid[0]))
+        cursor.execute("UPDATE game SET column{0}=column{1}-1 WHERE game_id={2}".format(column, column, gameid[0]))
 
     #check win
-    cursor.execute("SELECT row{0} FROM game WHERE game_id={1}".format(row, gameid))
+    cursor.execute("SELECT row{0} FROM game WHERE game_id={1}".format(row, gameid[0]))
     res = cursor.fetchone()
     if(num_moves % 2 == 0 and res[0] == board_size):
         win = 1
     elif(num_moves % 2 == 1 and res[0] == board_size * -1):
         win = 1
-    cursor.execute("SELECT column{0} FROM game WHERE game_id={1}".format(column, gameid))
+    cursor.execute("SELECT column{0} FROM game WHERE game_id={1}".format(column, gameid[0]))
     res = cursor.fetchone()
     if(num_moves % 2 == 0 and res[0] == board_size):
         win = 1
