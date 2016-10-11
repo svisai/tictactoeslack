92fec9ad9768973d13f55aeda7145f6ee0174766
Samidha Visai <svisai@umich.edu>
2016-10-10 21:35:55 -0400

    ending game with a win or tie

--- app.py
+++ app.py
@@ -106,18 +106,45 @@ def move(teamkey, channelkey, userkey, position):
         return 'Please wait your turn'
 
     board = list(board)
+    playernum = 0
     if(num_moves % 2 == 0):
         board[position] = 'X'
+        playernum = 1
     else:
         board[position] = 'O'
+        playernum = 2
 
     cursor.execute("SELECT board_size FROM game WHERE game_id = {0}".format(gameid[0]))
     boardsize = cursor.fetchone()
     boardsize = boardsize[0]
 
+    s = "".join(board)
+    cursor.execute("UPDATE game SET total_number_moves=total_number_moves+1 WHERE game_id={0}".format(gameid[0]))
+    cursor.execute("UPDATE game SET game_board='{0}' WHERE game_id={1}".format(s, gameid[0]))
+
+    res = ""
+    if(num_moves >= 3):
+        winner = checkwin(position, boardsize, gameid)
+    if(num_moves == 8 and winner == 0):
+        endgame(channelkey, teamkey)
+        res = 'There has been a tie. Game ended'
+
+    if(winner):
+        cursor.execute("UPDATE player SET total_wins=total_wins+1 WHERE player_id={0}".format(currentplayer[0]))
+        endgame(channelkey, teamkey)
+        res =  "<@{0}> won the game! Game over"
+    if(res != ""):
+        return printboard(teamkey, channelkey)
+
+    app.mysql.connection.commit()
+    cursor.close()
+
+    return res
+#increment for player1 (X) and decrement for player 2 (O)
+def checkwin(position, boardsize, gameid):
     column = position % boardsize
     row = position / boardsize
-
+    
     win = 0
     if(column == row):
         if(num_moves % 2 == 0):
@@ -129,7 +156,7 @@ def move(teamkey, channelkey, userkey, position):
         if(num_moves % 2 == 0 and res[0] == boardsize):
             win = 1
         elif(num_moves % 2 == 1 and res[0] == boardsize * -1):
-            win = 1
+            win = 2
 
 
     elif(column == boardsize - row - 1):
@@ -142,8 +169,8 @@ def move(teamkey, channelkey, userkey, position):
         if(num_moves % 2 == 0 and res[0] == boardsize):
             win = 1
         elif(num_moves % 2 == 1 and res[0] == boardsize * -1):
-            win = 1
-
+            win = 2
+    
     if(num_moves % 2 == 0):
         cursor.execute("UPDATE game SET row{0}=row{1}+1 WHERE game_id={2}".format(row, row, gameid[0]))
         cursor.execute("UPDATE game SET column{0}=column{1}+1 WHERE game_id={2}".format(column, column, gameid[0]))
@@ -157,22 +184,14 @@ def move(teamkey, channelkey, userkey, position):
     if(num_moves % 2 == 0 and res[0] == boardsize):
         win = 1
     elif(num_moves % 2 == 1 and res[0] == boardsize * -1):
-        win = 1
+        win = 2
     cursor.execute("SELECT column{0} FROM game WHERE game_id={1}".format(column, gameid[0]))
     res = cursor.fetchone()
     if(num_moves % 2 == 0 and res[0] == boardsize):
         win = 1
     elif(num_moves % 2 == 1 and res[0] == boardsize * -1):
-        win = 1
-        
-    if(win):
-        print 'winner'
-    s = "".join(board)
-    cursor.execute("UPDATE game SET total_number_moves=total_number_moves+1 WHERE game_id={0}".format(gameid[0]))
-    cursor.execute("UPDATE game SET game_board='{0}' WHERE game_id={1}".format(s, gameid[0]))
-    app.mysql.connection.commit()
-    cursor.close()
-    return printboard(teamkey, channelkey)
+        win = 2
+    return win
 
 def printboard(teamkey, channelkey):
     cursor = app.mysql.connection.cursor()
@@ -214,7 +233,6 @@ def printboard(teamkey, channelkey):
 
 def endgame(channelkey, teamkey):
     cursor = app.mysql.connection.cursor()
-    cursor.execute("DELETE FROM game WHERE channel_id = {0}".format(channelid[0]))
     cursor.execute("SELECT team_id FROM team WHERE team_key = '{0}'".format(teamkey))
     teamid = cursor.fetchone()
     cursor.execute("SELECT channel_id FROM channel WHERE team_id = {0} AND channel_key = '{1}'".format(teamid[0], channelkey))
